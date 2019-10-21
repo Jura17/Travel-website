ae129fe50b534a037457f5660b088b68ecb372b6
Jura17 <julian.rakow@web.de>
2019-10-21 19:45:07 +0200

    Basic landing page setup for mobile view

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
-  echo "How are you guys";
+
  ?>
 
- <h1>Hello everyone</h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Nihon Ni</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <img id="landing-page-img" src="img/logo-1.svg" alt="">
+    <div class="navigation">
+      <div class="login">
+        <h2>Login/Sign Up</h2>
+      </div>
+    </div>
+    <div class="main-container">
+      <h1>Connecting Japan travelers all around the world</h1>
+      <div class="">
+        <input type="button" name="new" value="Tell your story">
+      </div>
+      <div class="">
+        <input type="button" name="recent" value="Most recent story">
+      </div>
+      <div class="">
+        <input type="button" name="popular" value="Most popular story">
+      </div>
+      <div class="">
+        <input type="text" name="search" placeholder="Look up a story...">
+      </div>
+    </div>
+  </body>
+</html>
